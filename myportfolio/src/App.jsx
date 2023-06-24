27980f44a56dcfd09529c19048b345185ab8c49d
Jihee Kim <kimjihee1113@gmail.com>
2023-06-24 23:13:57 +0900

    로그인페이지완료 첫번째 * 빌드하기

--- myportfolio/src/App.jsx
+++ myportfolio/src/App.jsx
@@ -1,21 +1,12 @@
-import './App.css'
+import './style/App.css'
 import {Route, Routes} from "react-router-dom";
-import Main from "./Page/Main.jsx";
-import TemporaryDrawer from "./Page/TemporaryDrawer.jsx";
-import {createTheme, ThemeProvider} from "@mui/material";
 import Login from "./Page/Login/Login.jsx";
 import SignUpPage from "./Page/SignUp/SignUpPage.jsx";
-
+import {createTheme, ThemeProvider} from "@mui/material";
 function App() {
-    //MUI 다크모드
     const darkTheme = createTheme({
         palette: {
             mode: 'dark',
-        },
-        typography: {
-            fontFamily: [
-                'NanumSquareNeo-Variable'
-            ]
         },
     });
     return (
@@ -24,9 +15,6 @@ function App() {
                 <Routes>
                     <Route path="/" element={<Login/>}/>
                     <Route path="/signUp" element={<SignUpPage/>}/>
-
-
-                    <Route path="/sub" element={<TemporaryDrawer/>}/>
                 </Routes>
             </ThemeProvider>
         </div>
--- myportfolio/src/Page/Login/Login.jsx
+++ myportfolio/src/Page/Login/Login.jsx
@@ -1,6 +1,128 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
+import { BiLogIn } from "react-icons/bi";
+import {useState} from "react";
+import {IconButton, InputAdornment} from "@mui/material";
+
+
+function Copyright(props) {
+    return (
+        <Typography variant="body2" color="text.secondary" align="center" {...props}>
+            {'Copyright © '}
+            <Link color="inherit" href="https://github.com/HEAHEAA">
+               JIHEEGitHub
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
+}
 function Login(){
+    const [values, setValues] = useState({
+        showPass: false
+    })
+
+    const togglePasswordHide = () => {
+        setValues({
+            ...values,
+            showPass: !values.showPass
+        });
+    };
+
+
+
+
     return(
         <div>
+            <Box>
+                <Container component="main" maxWidth="xs" sx={{marginTop: 5}}
+                           style={{
+                    border: "1px solid #5b5e69",
+                    backgroundColor: "#2b2e38"
+                }}>
+                    <Box
+                        sx={{
+                            marginTop: 6,
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                        }}
+                    >
+                        <Typography component="h1" variant="h5" style={{
+                            fontFamily: 'kotra'
+                        }}>
+                            Login
+                        </Typography>
+                        <Typography component="small" variant="small">
+                            Welcome To My Portfolio!
+                        </Typography>
+                        <Box component="form"  noValidate sx={{ mt: 1 }}>
+                            <TextField
+                                margin="normal"
+                                required
+                                fullWidth
+                                label="ID"
+                                autoFocus
+                            />
+                            <TextField
+                                margin="normal"
+                                id="outlined-basic"
+                                type={values.showPass ? "text" : "password"}
+                                InputProps={{
+                                    endAdornment: (
+                                        <InputAdornment position="end">
+                                            <IconButton
+                                                aria-label="toggle password"
+                                                edge="end"
+                                                onClick={togglePasswordHide}
+                                            >
+                                                {values.showPass ? (
+                                                    <VisibilityIcon/>
+                                                ) : (
+                                                    <VisibilityOffIcon/>
+                                                )}
+                                            </IconButton>
+                                        </InputAdornment>
+                                    )
+                                }}
+                                label="password"
+                                variant="outlined"
+                                name="user_pwd"
+                                fullWidth
+                            />
+                            <FormControlLabel
+                                control={<Checkbox value="remember" color="primary" />}
+                                label="Remember me"
+                            />
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                sx={{ mt: 3, mb: 2 }}
+                                style={{
+                                    backgroundColor: "#4c5061",
+                                    color: "white"
+                                }}
+                            >
+                               Login <BiLogIn/>
+                            </Button>
+                        </Box>
+                        <p className="login-sign-p">
+                            회원가입이 필요하신가요?
+                        </p>
+                    </Box>
+                    <Copyright sx={{ mt: 8, mb: 4 }} />
+                </Container>
+            </Box>
 
         </div>
     )
